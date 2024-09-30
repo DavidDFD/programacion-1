36f17e964e55a35fea9d37aa9be537394d92275a
DavidDFD <davidfmontoya@estudiante.uniajc.edu.co>
2024-09-30 20:37:12 +0000

    finalizacion ejercicio 5.6

--- ejercicio56.java
+++ ejercicio56.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class ejercicio56 {
 
     public static void main(String[] args) {
-        System.out.println("mi primer programa de java en github");
+      
+// notas de los estudiantes 
+Scanner leer = new Scanner(System.in);
+System.out.print("Digite cantidad de estudiantes: ");
+int estudiantes = leer.nextInt();
+double nota = 0.0;
+int aprobados =0, noaprobados = 0;
+
+for(int i = 1; i<= estudiantes; i++){
+System.out.println("Nota estudiante" + i +": ");
+nota = leer.nextDouble();
+
+if (nota >=5.0){
+    aprobados++;
+
+}else{
+    noaprobados++;
+}
+}
+System.out.println("aprobados:" + aprobados + "noaprobados:" + noaprobados);
+
+
+
 
     }
 }
