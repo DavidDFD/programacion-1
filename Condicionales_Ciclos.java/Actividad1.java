f6836778f7cd9a457f02c144dacd14c6869f8f0d
DavidDFD <davidfmontoya@estudiante.uniajc.edu.co>
2024-10-07 21:11:56 +0000

    wuensa

--- Condicionales_Ciclos.java/Actividad1.java
+++ Condicionales_Ciclos.java/Actividad1.java
@@ -1,4 +1,4 @@
-import javax.print.DocFlavor.STRING;
+
 
 public class Actividad1 {
 
@@ -24,43 +24,36 @@ public class Actividad1 {
             } else {
                 System.out.println("fail");
                 System.out.println("mejor suerte para la proxima");
-        }
-        }
-
-
-
-
-
-// Actividad 3 Paki
-int india_score =300;
-int pakistan_score =290;
-
-if (india_score > pakistan_score)
-    System.out.println("La mejor nacion india gana");
-
-     else
-        System.out.println("La peor nacion gana pakistan");
-
-
-// Egercicio 3 tip 2
-int india = 300;
-int pakistan = 290;
-System.out.println(india < pakistan ?  "india gana" : "pakistan gana");
-
-// Egercicio 3.6
-int A = 7*3+6/2-5;
-int B = 21-8 + A% 3*11;
-if (A < B){
-    System.out.println(" a es menor que b");
-    }
-    if (A == B){
-        System.out.println("a es igual a b");
-         }
-         if (A > B){
-            System.out.println("b es mayor que a");
             }
+        }
 
+        // Actividad 3 Paki
+        int india_score = 300;
+        int pakistan_score = 290;
 
+        if (india_score > pakistan_score)
+            System.out.println("La mejor nacion india gana");
+
+        else
+            System.out.println("La peor nacion gana pakistan");
+
+        // Egercicio 3 tip 2
+        int india = 300;
+        int pakistan = 290;
+        System.out.println(india < pakistan ? "india gana" : "pakistan gana");
+
+        // Egercicio 3.6
+        int A = 7 * 3 + 6 / 2 - 5;
+        int B = 21 - 8 + A % 3 * 11;
+        if (A < B) {
+            System.out.println(" a es menor que b");
+        }
+        if (A == B) {
+            System.out.println("a es igual a b");
+        }
+        if (A > B) {
+            System.out.println("b es mayor que a");
+        }
 
     }
 
