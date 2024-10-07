a8c21192eafb9796ce9aa9ff89078bd21fbb6dd4
DavidDFD <davidfmontoya@estudiante.uniajc.edu.co>
2024-10-07 19:56:26 +0000

    sentecia

---
+++ Condicionales_Ciclos.java/Actividad1.java
@@ -0,0 +1,17 @@
+public class Actividad1{ 
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        boolean c = true;
+
+        if (!a || (b && c)) {
+            System.out.println("Sentencia if Ejecuada");
+        } else {
+            System.out.println("Sentencia else Ejecutada");
+
+        }
+
+}
+
+ }
