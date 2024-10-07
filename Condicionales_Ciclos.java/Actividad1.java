7aacb203d3257f0f8605737b583f938f3ac73a48
DavidDFD <davidfmontoya@estudiante.uniajc.edu.co>
2024-10-07 20:53:31 +0000

    renovacion 5

--- Condicionales_Ciclos.java/Actividad1.java
+++ Condicionales_Ciclos.java/Actividad1.java
@@ -1,4 +1,4 @@
-public class Actividad1{ 
+public class Actividad1 {
 
     public static void main(String[] args) {
         boolean a = true;
@@ -11,22 +11,40 @@ public class Actividad1{
             System.out.println("Sentencia else Ejecutada");
 
         }
-// Egercicio 2
+        // Egercicio 2
 
-int marks =80;
-if (marks >70){ 
-System.out.println("Distincion");
-if (marks >35){ 
-System.out.println("passs");
+        int marks = 80;
+        if (marks > 70) {
+            System.out.println("Distincion");
+            if (marks > 35) {
+                System.out.println("passs");
 
-    }else{
-        System.out.println("fail");
-        System.out.println("mejor suerte para la proxima");
+            } else {
+                System.out.println("fail");
+                System.out.println("mejor suerte para la proxima");
+        }
+        }
+
+
+
+
+
+// Actividad 3 Paki
+int india_score =300;
+int pakistan_score =290;
+
+if (india_score > pakistan_score)
+    System.out.println("La mejor nacion india gana");
+
+     else
+        System.out.println("La peor nacion gana pakistan");
+
+
+// Egercicio 3 tip 2
+int india = 300;
+int pakistan = 290;
+System.out.println(india < pakistan ?  "india gana" : "pakistan gana");
+
     }
-}
-
 
 }
-
- }
- 
